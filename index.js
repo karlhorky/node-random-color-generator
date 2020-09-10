eb521ed7ed80230ccf8dba7b25e9b57d452d6ece
Judith Kuneth <judith.kuneth@gmail.com>
2020-09-11 00:10:32 +0200

    Remove unused packages and unused variables

--- index.js
+++ index.js
@@ -1,38 +1,32 @@
-const randomColor = require('randomcolor'); // import the script
-const color = randomColor(); // a hex code for an attractive color
+const randomColor = require('randomcolor');
 const chalk = require('chalk');
 const prompt = require('prompt');
 
-const command = process.argv[2];
-const command2 = process.argv[3];
+const userInput = process.argv[2];
+const userInputLuminosity = process.argv[3];
 
-if (command === `ask`) {
+function output(colorCode) {
+  console.log(chalk.hex(colorCode)`
+  ################################
+  ################################
+  #######     ${colorCode}   ##########
+  ################################
+  ################################`);
+}
+
+if (userInput === `ask`) {
   prompt.start();
-  prompt.get(['usersColor', 'usersLuminosity'], function (err, result) {
-    const myColor = randomColor({
-      hue: result.usersColor,
+  prompt.get(['usersHue', 'usersLuminosity'], function (err, result) {
+    const color = randomColor({
+      hue: result.usersHue,
       luminosity: result.usersLuminosity,
     });
-
-    console.log(
-      chalk.hex(myColor)(`
-############################
-############################
-#######   ${myColor}  #########
-############################
-############################`),
-    );
+    output(color);
   });
-} else if (command !== 'ask') {
-  const command = process.argv[2];
-  const command2 = process.argv[3];
-  const usersPick = randomColor({ hue: command, luminosity: command2 });
-  console.log(
-    chalk.hex(usersPick)(`
-############################
-############################
-#######   ${usersPick}  #########
-############################
-############################`),
-  );
+} else if (userInput !== 'ask') {
+  const color = randomColor({
+    hue: userInput,
+    luminosity: userInputLuminosity,
+  });
+  output(color);
 }
